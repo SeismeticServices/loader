81a1bbaa0ef15c1a2798a1c0c5362f1471e4d693
GuestyGit <104333985+GuestyGit@users.noreply.github.com>
2023-05-01 21:08:56 -0800

    Add files via upload

---
+++ JS/FUCKGPT.js
@@ -0,0 +1,53 @@
+  var ligma = [
+  "https://example1.com",
+  "https://example2.com",
+  "https://example3.com"
+];
+
+function ligmaurls() {
+  var randomIndex = Math.floor(Math.random() * ligma.length);
+  window.location.href = ligma[randomIndex];
+}
+
+var seisurl = [
+  "https://west.desmondsmp.gq",
+	"https://singapore.desmondsmp.gq",
+  "https://ohio.desmondsmp.gq",
+  "https://eu.desmondsmp.gq"
+];
+
+function seismetic() {
+  var randomIndex = Math.floor(Math.random() * seisurl.length);
+  window.location.href = seisurl[randomIndex];
+}
+
+var fp = [
+  "https://example1.com",
+  "https://example2.com",
+  "https://example3.com"
+];
+
+function fileproxy() {
+  var randomIndex = Math.floor(Math.random() * fp.length);
+  window.location.href = fp[randomIndex];
+}
+
+var utopia = [
+  "https://utop.desmondsmp.gq"
+];
+
+function utop() {
+  var randomIndex = Math.floor(Math.random() * utopia.length);
+  window.location.href = utopia[randomIndex];
+}
+
+var int = [
+  "https://pleasebubgivememod.hahalol15.online/"
+];
+
+function interstellar() {
+  var randomIndex = Math.floor(Math.random() * int.length);
+  window.location.href = int[randomIndex];
+}
+
+
